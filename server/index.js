62a1cc4798331640e583a53f4268c9ca2645180c
unknown <Mohit_Pandey>
2026-05-13 00:09:17 +0530

    made some changes

--- server/index.js
+++ server/index.js
@@ -61,13 +61,11 @@ app.use("/posts", postRoutes);
 // mongoose setup
 
 const PORT = process.env.PORT || 6001;
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-    // adding data one time
-    // User.insertMany(users);
-    // Post.insertMany(posts);
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
+    );
   })
-  .catch((error) => console.log(`${error} did not connect`));
+  .catch((error) => console.log(error));
