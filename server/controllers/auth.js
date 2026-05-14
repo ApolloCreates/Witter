9cdbfbc57645166b92ca80cd9defca9a42c2af87
unknown <Mohit_Pandey>
2026-05-15 02:04:47 +0530

    added assets folder

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -34,9 +34,10 @@ export const register = async (req, res) => {
 
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  } catch (err) {
+  console.log(err);
+  res.status(500).json({ error: err.message });
+}
 };
 
 // login
