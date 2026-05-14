e6b7b5400a1a1c4f48b97e8d821f5bdb6a5d9a6c
unknown <Mohit_Pandey>
2026-05-15 01:53:46 +0530

    fixed frontend cors origin

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "https://witterapp.vercel.app",
+  origin: true,
   credentials: true
 }));
 app.use(helmet());
